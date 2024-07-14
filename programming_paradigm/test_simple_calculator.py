dd7b12093e6abe7b5cbfb5372b829e5b4d937684
hanzo1codes <bielertw@icloud.com>
2024-07-14 19:25:58 +0000

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,5 +1,6 @@
 import unittest
-# from simple_calculator import SimpleCalculator
+from simple_calculator import SimpleCalculator
+
 
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
